1e7f161e17f86e26c32587bdb255e17d6cf5beb0
vishal meshram <107854847+vishalboudhh@users.noreply.github.com>
2025-11-15 11:37:07 +0530

    new link

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -7,7 +7,7 @@ import profileImg from "../../assets/profile.jpg";
 const Home = () => {
   const handleDownloadCV = () => {
     window.open(
-      "https://drive.google.com/file/d/19QxxM39vs_pLpvpS4J1WgSuhy6XLTFFn/view?usp=sharing",
+      "https://drive.google.com/file/d/1gGAd2WckFDgOG3772SwZVsTLaDBG61rv/view?usp=sharing",
       "_blank"
     );
   };
